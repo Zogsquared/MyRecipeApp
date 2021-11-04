cc08bb70c0b7c74650b309dc72d73bdb4761feaa
ZogsquaredPC <73807715+Zogsquared@users.noreply.github.com>
2021-11-05 10:25:16 +1300

    Details
    NoteEntryPage update - ingredients and steps

--- MyRecipe/MyRecipe/MyRecipe/Models/Note.cs
+++ MyRecipe/MyRecipe/MyRecipe/Models/Note.cs
@@ -9,5 +9,7 @@ namespace MyRecipe.Models
         public int ID { get; set; }
         public string Text { get; set; }
         public string Author { get; set; }
+        public string Ingredients { get; set; }
+        public string Steps { get; set; }
     }
 }
